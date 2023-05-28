efa89f6cbaeb7402be236269cca9c9b4611f64cb
mkrey <mkrey@MARINA-DESKTOP>
2023-05-28 22:49:26 +0300

    Создан GasesCommand

---
+++ src/core/commands/gases_command/GasesCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lalalend_3.src.core.commands.gases_command
+{
+    internal class GasesCommand
+    {
+    }
+}
